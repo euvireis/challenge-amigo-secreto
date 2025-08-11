a7c5c8e26dec819699faca2062cca803817f9493
Victória Reis <victoriagr@gmail.com>
2025-08-11 22:42:01 +0100

    função agregar amigos

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 
-let nomes = []
+let nomes = [];
+
+function adicionarAmigo() {
+    let nomeDigitado = document.querySelector('input').value;
+    if (nomeDigitado == '') { 
+        alert("Por favor, insira um nome.");
+    } else {
+        nomes.push(nomeDigitado);
+    } 
+    limparCampo();
+} 
+ 
+function limparCampo() {
+    nomeDigitado = document.querySelector('input');
+    nomeDigitado.value = '';
+}
+
