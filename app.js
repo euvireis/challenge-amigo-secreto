c613401ce5f6e4614162a2c289bd2af2c193fbc8
Victória Reis <victoriagr@gmail.com>
2025-08-16 21:56:01 +0100

    Atualizar lista html

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+// O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. 
+// Aqui você deverá desenvolver a lógica para resolver o problema.
 
 let nomes = [];
 
@@ -10,10 +11,25 @@ function adicionarAmigo() {
         nomes.push(nomeDigitado);
     } 
     limparCampo();
+    atualizarLista();
 } 
- 
+
+
+
 function limparCampo() {
     nomeDigitado = document.querySelector('input');
     nomeDigitado.value = '';
 }
+
+function atualizarLista() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = ""
+
+    for(let i = 0; i < nomes.length; i++ ){
+        let item = document.createElement("li");
+        item.textContent = nomes[i];
+        lista.appendChild(item)
+    }
+}
+
 
