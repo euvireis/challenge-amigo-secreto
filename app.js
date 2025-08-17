e059cdf4008ee918aeae6a1c672dccb338103961
Victória Reis <victoriagr@gmail.com>
2025-08-17 23:11:57 +0100

    Atualizar jogo sorteio nomes

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 // O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. 
 // Aqui você deverá desenvolver a lógica para resolver o problema.
 
-let nomes = [];
+let amigos = [];
+let lista = document.getElementById("listaAmigos");
 
 function adicionarAmigo() {
     let nomeDigitado = document.querySelector('input').value;
     if (nomeDigitado == '') { 
         alert("Por favor, insira um nome.");
     } else {
-        nomes.push(nomeDigitado);
+        amigos.push(nomeDigitado);
     } 
     limparCampo();
     atualizarLista();
@@ -22,14 +23,25 @@ function limparCampo() {
 }
 
 function atualizarLista() {
-    let lista = document.getElementById("listaAmigos");
     lista.innerHTML = ""
-
-    for(let i = 0; i < nomes.length; i++ ){
+    for(let i = 0; i < amigos.length; i++ ){
         let item = document.createElement("li");
-        item.textContent = nomes[i];
+        item.textContent = amigos[i];
         lista.appendChild(item)
     }
 }
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("Por favor, insira um nome.");
+    } else {
+       let indice = Math.floor(Math.random() * amigos.length);
+       let nomeSorteado = amigos[indice];
+        let nomeResultado = document.getElementById("resultado");
+        nomeResultado.innerHTML = (`O amigo escolhido foi: ${nomeSorteado}`);
+        lista.innerHTML = ""
+    }
+}
+
 
 
